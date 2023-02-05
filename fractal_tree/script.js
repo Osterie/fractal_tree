201ccea7be2054e9b4a6282ddb9f34876fa665a6
OSTERITE <adrian@bandidos.org>
2023-02-05 21:38:52 +0100

    simple version

--- fractal_tree/script.js
+++ fractal_tree/script.js
@@ -1,60 +1,57 @@
-const button = document.getElementById('button')
+const create_tree_button = document.getElementById('create_tree')
+
+const get_xstart = parseInt(document.getElementById("xstart").value)
+const get_ystart = parseInt(document.getElementById("ystart").value)
+
+const get_start_length = parseInt(document.getElementById("length").value)
+const get_length_change_expression = document.getElementById("length_change_expression").value
+
+const get_angle = parseInt(document.getElementById("angle").value)
+const get_iterations = parseInt(document.getElementById("iterations").value)
+
+create_tree_button.addEventListener('click', () =>{
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    const get_xstart = parseInt(document.getElementById("xstart").value)
+    const get_ystart = parseInt(document.getElementById("ystart").value)
+
+    const get_start_length = parseInt(document.getElementById("length").value)
+    const get_length_change_expression = document.getElementById("length_change_expression").value
+
+    const get_angle = parseInt(document.getElementById("angle").value)
+    const get_iterations = parseInt(document.getElementById("iterations").value)
 
 
-button.addEventListener('click', () =>{
-    button_handler(input_field)
+    draw_fractal(get_xstart, get_ystart, get_start_length, get_length_change_expression, get_angle, get_iterations)
 })
-
-
-function button_handler(input) {
-    const input_verdi = input.value
-    output.innerHtml = 'output'
-    //do stuff
-}
-
 
 
 let canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
-// ctx.lineWidth = 15;
-
-function name(params) {
-    
-}
-
-
-
 window.onload = winInit;
 function winInit() {
-
-    draw_fractal(xstart, ystart, length, angle, iterations)
-    //do stuff
+    draw_fractal(get_xstart, get_ystart, get_start_length, get_length_change_expression, get_angle, get_iterations)
 }
 
 
 
 
 //iterations?
-const xstart = canvas.width/2
-const ystart = canvas.height - 50;
-const length = 120
-const angle = 45
-const iterations = 10
 
-function draw_fractal(xstart, ystart, length, angle, iterations){
+
+function draw_fractal(xstart, ystart, length, length_change_expression, angle, iterations){
+
     ctx.beginPath();
     ctx.save()
 
-    var num = Math.round(4*length*length-80*length+500);
+    // var num = Math.round(4*length*length-80*length+500);
     // ctx.strokeStyle = `hsl(${length*2+200}, ${50}%, ${50}%)`;
-
     // ctx.strokeStyle = `hsl(${fits(255, iterations)}, ${75}%, ${50}%)`;
-    ctx.strokeStyle = `rgb(${fits(255, length/50)}, ${fits(255, iterations)}, ${0})`;
- 
 
-    // console.log(fits(255, length))
-    
+    ctx.strokeStyle = `rgb(${fits(255, length/50)}, ${fits(255, iterations)}, ${0})`;
+
     
     ctx.lineWidth = length/10
 
@@ -70,8 +67,9 @@ function draw_fractal(xstart, ystart, length, angle, iterations){
         ctx.restore();
         return;
     }
-    draw_fractal(0, -length, length*0.8, -angle, iterations-1)
-    draw_fractal(0, -length, length*0.8, +angle, iterations-1)
+
+    draw_fractal(0, -length, Function( `return ${length_change_expression.replace(/L/g, length)}` )(), length_change_expression, -angle, iterations-1)
+    draw_fractal(0, -length, Function( `return ${length_change_expression.replace(/L/g, length)}` )(), length_change_expression, +angle, iterations-1)
 
     ctx.restore();
 }
